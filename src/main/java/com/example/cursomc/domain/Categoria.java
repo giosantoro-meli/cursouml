f399a30df90e14632bce1d736d403ab45352d085
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-09-15 09:22:11 -0300

    Testando classe de domínio Categoria

---
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -0,0 +1,45 @@
+package com.example.cursomc.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
